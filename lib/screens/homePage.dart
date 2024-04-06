b7804d8ab6b7a3222a55a0a38bc470cdd3e7ffe7
favourTy <favouradetayo03@gmail.com>
2024-04-06 06:08:01 +0100

    HomePage

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -11,16 +11,57 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   void initState() {
+
     super.initState();
+    initSpeech();
   }
   void  initSpeech ()async{
-    await _speechToText.initialize();
+     speechEnabled = await _speechToText.initialize();
+     setState(() {
+       
+     });
   }
+
+  void startListening() async{
+       await _speechToText.listen(onResult:_onSpeechResult );
+  }
+
+  void _onSpeechResult (result){
+    setState(() {
+      _wordSpoken = "${result.recognizedWords}";
+      _coonfidenceLevel = result.confidence;
+    });
+  }
+
    final SpeechToText _speechToText = SpeechToText();
+   bool speechEnabled = false;
+   String _wordSpoken = "";
+   int _coonfidenceLevel = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+    appBar:  AppBar(
+      backgroundColor: Colors.purple,
+      title: const Text("VoiceNova",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 25,
+      ),
+      
+      ),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(12.0),
+            child: Text(_speechToText.isListening ?"Listening": speechEnabled? "Tap the microphone to start listening": "Speech is not available"),
 
+          )
+        ],
+      ),
+    ),
     );
   }
 }
